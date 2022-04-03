95a3c31d0c7a16cd4cfb0f65682190dcfc621e7f
hienhhcc <thachdau16t@gmail.com>
2022-04-03 12:00:18 +0700

    update UI displaybox

--- src/components/DisplayBox/index.js
+++ src/components/DisplayBox/index.js
@@ -1,17 +1,29 @@
 import { useContext } from 'react';
 import { ThemeContext } from '../../context/theme';
-import { StyledDisplayBox, StyledSpan } from './styles';
+import {
+  StyledDisplayBox,
+  StyledDisplayValue,
+  StyledPrevious,
+  StyledSpan,
+} from './styles';
 
 const DisplayBox = ({ input }) => {
   const { theme } = useContext(ThemeContext);
 
   return (
     <StyledDisplayBox customTheme={theme}>
-      {input.map((i) => (
-        <StyledSpan key={i + Math.random()} customTheme={theme}>
-          {i}
-        </StyledSpan>
-      ))}
+      <StyledPrevious customTheme={theme}>
+        {input.length > 1
+          ? input.map((i) => (
+              <StyledSpan key={i + Math.random()} customTheme={theme}>
+                {i}
+              </StyledSpan>
+            ))
+          : null}
+      </StyledPrevious>
+      <StyledDisplayValue customTheme={theme}>
+        {input.length <= 2 ? input[0] : input[2]}
+      </StyledDisplayValue>
     </StyledDisplayBox>
   );
 };
--- src/components/DisplayBox/styles/index.js
+++ src/components/DisplayBox/styles/index.js
@@ -6,8 +6,10 @@ export const StyledDisplayBox = styled.div`
   text-align: right;
 
   display: flex;
-  align-items: center;
-  justify-content: end;
+  flex-direction: column;
+  gap: 0.1rem;
+  align-items: flex-end;
+  justify-content: center;
   padding: 0.7rem;
   color: #333;
   font-weight: bold;
@@ -15,6 +17,17 @@ export const StyledDisplayBox = styled.div`
   font-size: 1.5rem;
   border-radius: 10px;
   height: 140px;
+`;
+
+export const StyledPrevious = styled.div`
+  color: ${(props) => props.customTheme.textHeaderDisplay};
+  font-size: 1.1rem;
+  opacity: 0.5;
+`;
+export const StyledDisplayValue = styled.div`
+  color: ${(props) => props.customTheme.textHeaderDisplay};
+  justify-self: flex-end;
+  opacity: 1;
 `;
 
 export const StyledSpan = styled.span`
